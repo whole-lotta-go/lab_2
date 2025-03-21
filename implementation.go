38df93cdc9ce1cfcb8009bf7971c59de7b772df1
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2025-03-21 16:22:05 +0200

    Update function's signature to match team's variant

--- implementation.go
+++ implementation.go
@@ -4,6 +4,6 @@ import "fmt"
 
 // TODO: Document this function.
 // EvalPostfix evaluates
-func EvalPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func EvalPostfix(input string) (int, error) {
+	return 0, fmt.Errorf("TODO")
 }
